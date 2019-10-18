a452dc3b251d701c7bdf42e72fff99914543ef7d
mvaload <mvaload@gmail.com>
2019-10-18 10:10:15 +0300

    added controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', [
+            'users' => $users,
+            'initIteration' => ($users->currentPage() - 1) * $users->perPage()
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        return view('users.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'password' => ['nullable', 'string', 'min:8', 'confirmed']
+        ]);
+        auth()->user()->name = $data['name'];
+        
+        if ($data['password']) {
+            auth()->user()->password = Hash::make($data['password']);
+        }
+        auth()->user()->save();
+        return redirect()->route('home')->with('status', 'Your account has been updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $user = auth()->user();
+        auth()->guard()->logout();
+        $request->session()->invalidate();
+        $user->delete();
+        return redirect()->route('home');
+    }
+}
