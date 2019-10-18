cba026ec0f06f8aae846ef6451ce3889e96d5cd0
mvaload <mvaload@gmail.com>
2019-10-18 10:17:11 +0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes(['reset' => false]);
+Route::get('/', 'IndexController@index')->name('home');
+Route::get('/users', 'UserController@index')->name('users.index');
+Route::get('/users/edit', 'UserController@edit')->name('users.edit');
+Route::put('/users', 'UserController@update')->name('users.update');
+Route::delete('/users', 'UserController@destroy')->name('users.destroy');
