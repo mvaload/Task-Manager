278f37c5b04fb8219c3d2282641569cdb06d163e
mvaload <mvaload@gmail.com>
2019-10-29 10:32:30 +0300

    added tags check list

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -58,9 +58,18 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Tags') }}</label>
+                            <label for="tags" class="col-md-4 col-form-label text-md-right">{{ __('Tags') }}</label>
                             <div class="col-md-6">
-                                <input id="tags" type="text" class="form-control" name="tags" value="{{ old('tags') }}">
+                            <select id="tags" class="form-control @error('tags') is-invalid @enderror" name="tags">
+                                @foreach ($tags as $tag)
+                                <option value="{{ $tag->id }}" {{ old('tags') == $tag->id ? ' selected' : '' }}>{{ $tag->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('tags')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                             </div>
                         </div>
                         <div class="form-group row mb-0">
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -60,9 +60,19 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Tags') }}</label>
+                            <label for="tags" class="col-md-4 col-form-label text-md-right">{{ __('Tags') }}</label>
                             <div class="col-md-6">
-                                <input id="tags" type="text" class="form-control" name="tags" value="{{ $task->tags->implode('name', ', ') }}">
+                                <select id="tags" class="form-control @error('tags') is-invalid @enderror" name="tags">
+                                    <option>
+                                        @foreach ($tags as $tag)
+                                    <option value="{{ $tag->id }}" {{ $task->id == $tag->id ? ' selected' : '' }}>{{ $tag->name }}</option>
+                                    @endforeach
+                                </select>
+                                @error('tags')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </div>
                         <div class="form-group row mb-0">
